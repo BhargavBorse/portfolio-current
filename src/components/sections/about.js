1caf69861e8ddb035a8d07a6d01df232c8322819
Bhargav Borse <bhargavborse4898@gmail.com>
2021-05-02 10:16:53 +0530

    updated about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -117,7 +117,7 @@ const StyledPic = styled.div`
 const About = () => {
   const data = useStaticQuery(graphql`
     query {
-      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "me.jpg" }) {
+      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "3F34409F-222F-4924-BEC5-EBE9B6F138D5.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 500, traceSVG: { color: "#64ffda" }) {
             ...GatsbyImageSharpFluid_withWebp_tracedSVG
@@ -138,7 +138,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'React', 'Eleventy', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'Gatsby', 'PHP', 'PWA', 'Node.js', 'Firebase'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -148,21 +148,17 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
+              Hello! My name is Bhargav and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2016 when I decided to try editing custom
               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              about JavaScript, HTML &amp; CSS!
             </p>
 
             <p>
-              Fast-forward to today, and I've had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I've had the privilege to be a part of various IT 
+              communities such as <a>Mozilla Gujarat</a>, <a>Karigar</a>, <a> Wordpress Developers Gujarat </a> and <a> Microsoft Student 
+                Community</a>. My main focus these days is building accessible, 
+              inclusive products and digital experiences for a variety of clients.
             </p>
 
             <p>Here are a few technologies I've been working with recently:</p>
