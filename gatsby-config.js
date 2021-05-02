075a0def7b8fe2318a31c15dbd1cc2bd74efbd10
Bhargav Borse <bhargavborse4898@gmail.com>
2021-05-02 12:37:52 +0530

    Google site verification

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Bhargav Borse',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Bhargav Borse is a Web Developer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://bhargavborse.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Bhargav Borse',
+        short_name: 'Bhargav Borse',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
